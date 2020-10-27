2d58f6a9bcdc9564566aa1ae9bdcf9aff3a15267
TalonFlame1 <72523652+TalonFlame1@users.noreply.github.com>
2020-10-26 17:59:25 -0700

    Add files via upload

---
+++ log.js
@@ -0,0 +1,28 @@
+class Log{
+
+    constructor (x,y,height,angle){
+    var options = {
+        restitution: 0.5,
+        friction: 1.0
+    }
+    this.body = Bodies.rectangle (x,y,20,height,options)
+    World.add (world,this.body)
+    this.width = 20
+    this.height = height
+    Matter.Body.setAngle(this.body,angle)
+    }
+    
+    display () {
+    var pos = this.body.position
+    var angle = this.body.angle
+    push()
+    translate(pos.x,pos.y)
+    rotate (angle)
+    fill("brown")
+    strokeWeight(5)
+    stroke("black")
+    rectMode (CENTER)
+    rect (0,0,this.width,this.height)
+    pop ()
+    }
+}
